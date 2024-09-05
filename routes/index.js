1faac230e6355169405e4864d7ec8a5def68fb5d
sachingupta890 <sachinvarshney459@gmail.com>
2024-09-05 18:36:34 +0530

    updated

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userRoute from "../routes/user.js"
-const router = express.Router();
-
-
-router.use('/user', userRoute);
-
-export default router;
--- utils/utilities-class.js
+++
@@ -1,9 +0,0 @@
-
-class ErrorHandler extends Error {
-  constructor(message = "Internal Server Error", statusCode = 500) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = ErrorHandler;
